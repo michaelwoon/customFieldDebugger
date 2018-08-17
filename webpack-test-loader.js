a401922dffa03d5520a9b4423e671bba23befcfb
mwoon <mwoon@yext.com>
2018-08-17 09:33:49 -0400

    Loader uses soy2js

--- webpack-test-loader.js
+++ webpack-test-loader.js
@@ -1,8 +1,30 @@
-//remove console.log statements
-module.exports = function(source, map) {
-    this.callback(
-        null,
-        source.replace(/console\.log\(.*\);?\n/g, 'michaelwoon'),
-        map
-    );
-};
+path = require('path');
+const fs = require('fs');
+
+module.exports = function(source) {
+
+    gets the soy2js js file
+    let inputDir = './templates';
+    let outputDir = './.tmp/js/templates';
+    let resource = this.resourcePath;
+    resource = path.relative(inputDir, resource);
+    resource = resource.replace(".soy", ".js");
+    resource = `${outputDir}/${resource}`;
+    var jsSource = fs.readFileSync(resource, 'utf8');
+
+    //set regex
+    var re = /cftest="(.*?)"/g;
+    var newSource = source;
+    var cfName = re.exec(newSource); //finds first instance of cfname tag
+
+    while(cfName)
+    {
+      var newHtml = 'title="' + cfName[1] + '" style="border: 1px dashed red"';
+      //replaces cftag tag stuff with styling
+      newSource = newSource.replace(/cftest="[A-Za-z0-9]*"/, newHtml);
+      cfName = re.exec(newSource); //get next instance of tag
+    }
+    console.log(newSource);
+
+    return newSource;
+}
